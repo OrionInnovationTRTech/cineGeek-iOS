de1864f6ba99a8e32256b7cd1540fcfe02daf686
berkay079 <94804286+berkay079@users.noreply.github.com>
2022-05-19 23:52:56 +0300

    -first base version

---
+++ CineGeek/CineGeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineGeekApp.swift
+//  CineGeek
+//
+//  Created by Berkay YILDIRIM on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CineGeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
